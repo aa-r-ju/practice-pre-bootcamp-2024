93a163431967c2117326b5c14b1d58dc82230810
aa-r-ju <aarju3447@gmail.com>
2024-07-29 19:37:56 +0545

    feat:completed 01.overview and set up

--- bootcamp-prep-main/pre-work-workshops/01-overview-and-setup/01-hello-world/hello-world.js
+++ bootcamp-prep-main/pre-work-workshops/01-overview-and-setup/01-hello-world/hello-world.js
@@ -1,4 +1,4 @@
-console.log('hello world!');
+console.log("hello world!");
 
 // this is a comment!
 
@@ -6,3 +6,8 @@ console.log('hello world!');
   this is also a comment!
   it spans multiple lines!
 */
+let greeting = "Hello";
+let name = "world";
+let me = "i am Aarju";
+let add = greeting + " " + name + " " + me;
+console.log(add);
--- bootcamp-prep-main/pre-work-workshops/01-overview-and-setup/02-simple-sum/simple-sum.js
+++ bootcamp-prep-main/pre-work-workshops/01-overview-and-setup/02-simple-sum/simple-sum.js
@@ -1,1 +1,2 @@
 console.log(10 + 20);
+console.log((500 / 5) * 2);
